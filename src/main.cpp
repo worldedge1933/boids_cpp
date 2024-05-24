82e5c04bf392326e40b7807ac8a8f9aa4c593546
jiashenzhang1933 <jiashenzhang1933@outlook.com>
2024-05-24 20:00:15 +0800

    主要页面逻辑

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,63 @@
 
 const int WIDTH = 1600;
 const int HEIGHT = 900;
+bool running = true;
+
+
+bool mainWindow() {
+    while (true) {
+        cleardevice();
+        setfillcolor(RGB(255, 0, 0));
+
+        solidrectangle(500, 300, 1100, 450);
+        solidrectangle(500, 500, 1100, 650);
+
+        FlushBatchDraw();
+        ExMessage msg;
+        while (peekmessage(&msg)) {
+            if (msg.message == WM_LBUTTONUP && msg.x >= 500 && msg.x <= 1100 && msg.y >= 300 && msg.y <= 450) {
+                return true;
+            } else if (msg.message == WM_LBUTTONUP && msg.x >= 500 && msg.x <= 1100 && msg.y >= 500 && msg.y <= 650) {
+                return false;
+            }
+
+        }
+
+    }
+
+}
+
+void free_boids() {
+    while (true) {
+        cleardevice();
+        rectangle(46, 34, 60, 50);
+        FlushBatchDraw();
+    }
+}
+
+void controlled_boids() {
+    while (true) {
+        cleardevice();
+        circle(46, 34, 20);
+        FlushBatchDraw();
+    }
+
+}
 
 int main() {
-    bool running = true;
     initgraph(WIDTH, HEIGHT);
+    BeginBatchDraw();
+
     circle(320, 240, 100);
+
     while (running) {
-        Sleep(200);
+        if (mainWindow()) {
+            free_boids();
+        } else {
+            controlled_boids();
+        }
     }
+
+    EndBatchDraw();
     return 0;
 }
